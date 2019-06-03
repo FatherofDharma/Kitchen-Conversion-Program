dadc68fe0eb2cb931421281b04ef0039e5989bb2
Daniel Hongel, dominic montelongo and Mia Murdock <mia@>
2019-06-03 16:11:29 -0700

    Add js strutcure for conversion of gallons to oz

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+var gallons = parseFloat(prompt("Enter Amount in gallons."));
+
+var oz = (gallons * 128);
+
+alert (("Converts to ")+(oz)+("ounces"));
+
+console.log(gallons);
+console.log(oz);
+
+// var  unitType = prompt ("Enter units.");
